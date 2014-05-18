b0b87f87176ba1fb1ff9e9e427ac7e646f536fa9
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2014-05-18 21:34:43 +0200

    + generate tasks & check result

--- MatheTest/web/main.dart
+++ MatheTest/web/main.dart
@@ -3,6 +3,8 @@ library math_test;
 import 'package:angular/angular.dart';
 import 'package:angular/application_factory.dart';
 
+import 'math_task.dart';
+
 /**
  * GUI controller for MatheTest app
  */
@@ -14,6 +16,11 @@ class MathTestGuiController {
   // public properties
   int numTasks;
   bool isRunning;
+  String successMsg;
+  List<MathTask> tasks;
+  
+  // private properties
+  int _correctTasks;
   
   // constructor
   MathTestGuiController() {
@@ -23,23 +30,55 @@ class MathTestGuiController {
   // public methods
   void startTest() {
     isRunning = true;
+    
+    tasks.clear();
+    
+    // generate tasks
+    for (var i=0; i<numTasks; i++) {
+      tasks.add(new MathTask('+', 100));
+    }
   }
   
   void stopTest() {
     isRunning = false;
+    _correctTasks = 0;
+    
+    for (var i=0; i<numTasks; i++) {
+      if (tasks[i].checkResult()) {
+        _correctTasks++;
+      }
+    }
+    
+    _successMsg();
   }
   
   // private mathods
   void _defaultData() {
     isRunning = false;
     numTasks = 10;
+    tasks = [];
+    successMsg = '';
+    _correctTasks = 0;
+  }
+  
+  void _successMsg() {
+    var ratio = _correctTasks/numTasks * 100;
+    if (ratio < 50) {
+      successMsg = "Das war wohl nichts! Nur ${_correctTasks} Aufgaben von ${numTasks} sind richtig. Am besten gleich noch mal versuchen...";
+    } else if (ratio < 75) {
+      successMsg = "Gar nicht so schlecht: ${_correctTasks} von ${numTasks} Aufgaben sind richtig.";
+    } else if (_correctTasks == numTasks) {
+      successMsg = "PERFECT! Alles richtig!";
+    } else {
+      successMsg = "Sehr gut!! ${_correctTasks} Aufgaben von ${numTasks} sind richtig!";
+    }
   }
 }
 
 
 class MathTestModule extends Module {
   // constructor
-  MandelbrotModule() {
+  MathTestModule() {
     type(MathTestGuiController);
   }
 }
--- MatheTest/web/math_task.dart
+++ MatheTest/web/math_task.dart
@@ -10,18 +10,58 @@ class MathTask {
   // public properties
   int operand_1;
   int operand_2;
+  int result;
   String operator;
+  String trial;
+  bool correct;
   
   // private properties
   static Random _rand = new Random();
+  int _upperLimit;
   
   // constructor
-  MathTask() {
+  // TODO: hand-over list of allowed operators
+  MathTask(String op, [int maxValue]) {
+    
+    trial = "";
+    correct = true;
+    
+    // TODO: randomly select operator from given list
+    operator = op;
+    if (maxValue != null) { _upperLimit = maxValue; }
+    else { _upperLimit = 100; }
+    
+    switch (operator) {
+      case '+':
+        result = _getNumber(1, _upperLimit);
+        operand_1 = result - _getNumber(0, result);
+        operand_2 = result - operand_1;
+        break;
+      case '-':
+        operand_1 = _getNumber(1, _upperLimit);
+        operand_2 = operand_2 - _getNumber(0, operand_1);
+        result = operand_1 - operand_2;
+        break;
+      default:
+        throw "Unsuported operator $operator";
+    }
+  }
+  
+  // check trial
+  bool checkResult() {
+    try {
+      var trialNum = int.parse(trial);
+      if (trialNum == result) correct = true;
+      else correct = false;
+    } on FormatException {
+      correct = false;
+    }
+    
+    return correct;
   }
   
   // private methods
   int _getNumber(int min, int max) {
-    
-    return 1;
+    return (min + _rand.nextInt(max-min));
   }
 }
