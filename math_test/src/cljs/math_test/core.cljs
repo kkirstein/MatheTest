9f4be6197df6f25ab0c02d49a49f2a7412df8678
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2015-01-13 21:55:37 +0100

    + setup basic skeleton

--- math_test/src/cljs/math_test/core.cljs
+++ math_test/src/cljs/math_test/core.cljs
@@ -2,42 +2,60 @@
     (:require [reagent.core :as reagent :refer [atom]]))
 
 ;; -------------------------
+;; Model
+
+(defn initial-state []
+  {:config {:upper-limit 100,
+            :operands #{:add :sub :mul :div},
+            :positions #{2 3}}
+   :tasks []})
+
+
+(defn new-task
+  "Generate a new task with given configuration."
+  [config]
+  (let [upper-limit (:upper-limit config)
+        operands (:operands config)
+        positions (:positions config)]
+    (merge (cond
+             (operands :add) {:operand-1 1, :operand-2 2, :result 3}
+             (operands :sub) {:operand-1 1, :operand-2 2, :result 3}
+             (operands :mul) {:operand-1 1, :operand-2 2, :result 3}
+             (operands :div) {:operand-1 1, :operand-2 2, :result 3})
+           {:trial "", :correct true})))
+
+
+(def app-state (atom (initial-state)))
+
+;; -------------------------
 ;; Views
 
-(defn home-page []
-  [:div [:h2 "Welcome to math_test"]
-   [:div [:a {:href "#/about"} "go to about page"]]])
+(defn task-pane []
+  [:div [:h1 "Aufgaben"]
+   [:ul
+    [:li "Aufgabe 1"]
+    [:li "Aufgabe 2"]]])
 
-(defn about-page []
-  [:div [:h2 "About math_test"]
-   [:div [:a {:href "#/"} "go to the home page"]]])
+(defn control-pane []
+  [:div
+   [:ul
+    [:li "Anzahl Aufgaben"]
+    [:li "Start / Stop"]]])
 
-(defn current-page []
-  [:div [(session/get :current-page)]])
+(defn main-page []
+  [:div [:h1 "Mathe-Test"]
+   task-pane
+   control-pane])
+
 
 ;; -------------------------
 ;; Routes
-(secretary/set-config! :prefix "#")
 
-(secretary/defroute "/" []
-  (session/put! :current-page home-page))
+; none
 
-(secretary/defroute "/about" []
-  (session/put! :current-page about-page))
 
 ;; -------------------------
 ;; Initialize app
 (defn init! []
-  (reagent/render-component [current-page] (.getElementById js/document "app")))
+  (reagent/render-component [main-page] (.getElementById js/document "app")))
 
-;; -------------------------
-;; History
-(defn hook-browser-navigation! []
-  (doto (History.)
-    (events/listen
-     EventType/NAVIGATE
-     (fn [event]
-       (secretary/dispatch! (.-token event))))
-    (.setEnabled true)))
-;; need to run this after routes have been defined
-(hook-browser-navigation!)
