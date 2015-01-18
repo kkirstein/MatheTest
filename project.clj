f640fd38c9b0e4c50ef837b55e0413bc7b01d1a1
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2015-01-18 21:31:13 +0100

    ! upate license info

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject math-test "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "3-clause BSD-style License"
+            :url ""}
 
   :source-paths ["src" "dev"]
 
