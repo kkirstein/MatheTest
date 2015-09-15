5371785ae4cc81ec71dcf2f74d18453790a3d6f8
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2015-09-15 21:38:27 +0200

    ! name tag file as .js

--- js/math_test.js
+++ js/math_test.js
@@ -1,4 +1,13 @@
-riot.tag('math-test', '<div id="mathtest-gui"> <h1>Mathe Test</h1> </div> <div id="mathtest-tasks"> </div>', function(opts) {
+<!-- vim: ft=html sw=2 ts=2
+-->
+<math-test>
 
-});
+	<div id="mathtest-gui">
+		<h1>Mathe Test</h1>
+	</div>
+
+	<div id="mathtest-tasks">
+
+	</div>
+</math-test>
 
